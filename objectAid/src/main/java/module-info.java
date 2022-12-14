1d6564584b3096301c90033e3b3be104dc833f86
Alexis Chân Gridel <alexis.gridel@pm.me>
2022-12-14 16:01:58 +0100

    feat: initial commit javaFx project

---
+++ objectAid/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.teamtree.objectaid {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens org.teamtree.objectaid to javafx.fxml;
+    exports org.teamtree.objectaid;
+}
