70a3d08101f82e743ed8c70c33e5ff185b921e83
Flotss <manginf54@gmail.com>
2022-12-15 08:51:44 +0100

    Ajout Attribut

---
+++ objectAid/src/main/java/org/teamtree/objectaid/Attribut.java
@@ -0,0 +1,34 @@
+package main.java.org.teamtree.objectaid;
+
+import org.teamtree.objectaid.Etat;
+import org.teamtree.objectaid.Accessibilite;
+
+public class Attribut {
+    private final String type;
+    private final String nom;
+    private final Etat etat;
+    private final Accessibilite accessibilite;
+
+    public Attribut(String type, String nom, Etat etat, Accessibilite accessibilite) {
+        this.type = type;
+        this.nom = nom;
+        this.etat = etat;
+        this.accessibilite = accessibilite;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getEtat() {
+        return etat.getEtat();
+    }
+
+    public String getAccessibilite() {
+        return accessibilite.getAcces();
+    }
+}
