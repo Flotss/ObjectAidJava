8b760fc637e49cc3e129eb7cce1edc51bb1946ce
Alexis Chân Gridel <alexis.gridel@pm.me>
2022-12-15 09:42:25 +0100

    feat(dto): ClasseEntiere initial commit

--- objectAid/src/main/java/org/teamtree/objectaid/ClasseEntiere.java
+++ objectAid/src/main/java/org/teamtree/objectaid/ClasseEntiere.java
@@ -1,9 +1,32 @@
 package org.teamtree.objectaid;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ClasseEntiere {
-    public DefinitionClasse definitionClasse;
-    public ArrayList<Methode> methodes;
-    public ArrayList<Attribut> attributs;
+    private final String name;
+    private final List<Attribut> attributes;
+    private final List<Methode> methods;
+
+    public ClasseEntiere(final String name) {
+        this.name = name;
+        this.attributes = new ArrayList<>();
+        this.methods = new ArrayList<>();
+    }
+
+    public void deplacer(final int x, final int y) {
+        // TODO
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Attribut> getAttributes() {
+        return attributes;
+    }
+
+    public List<Methode> getMethods() {
+        return methods;
+    }
 }
