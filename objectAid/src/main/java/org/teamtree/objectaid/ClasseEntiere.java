7cfe6f80200997018669c6f082d4e47821775887
Julien Bernard <julienbernardjb88@gmail.com>
2022-12-15 08:42:08 +0100

    ClasseEntiere début

---
+++ objectAid/src/main/java/org/teamtree/objectaid/ClasseEntiere.java
@@ -0,0 +1,11 @@
+package org.teamtree.objectaid;
+
+import java.util.ArrayList;
+
+public class ClasseEntiere {
+    public DefinitionClasse definitionClasse;
+    public ArrayList<Methode> methodes;
+    public ArrayList<Attribut> attributs;
+
+
+}
