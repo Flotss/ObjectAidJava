fd3eb4f198f5fa3033b91224f01f93d94234fa99
Flotss <manginf54@gmail.com>
2022-12-16 08:45:10 +0100

    Test constructeur

---
+++ objectAid/src/test/java/org/teamtree/objectaid/classes/ConstructeurTest.java
@@ -0,0 +1,78 @@
+package org.teamtree.objectaid.classes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.teamtree.objectaid.Classe.Constructeur;
+import org.teamtree.objectaid.Classe.Methode;
+import org.teamtree.objectaid.Classe.Parametre;
+import org.teamtree.objectaid.Etat.Etat;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class ConstructeurTest {
+
+    Constructeur[] constructeur;
+
+
+    @BeforeEach
+    public void setUp() throws ClassNotFoundException {
+        Class<?> c = Class.forName("org.teamtree.objectaid.classes.ClassePourLesTests");
+        Constructor<?>[] constructeurs = c.getDeclaredConstructors();
+        constructeur = new Constructeur[constructeurs.length];
+        for (int i = 0; i < constructeurs.length; i++) {
+            constructeur[i] = new Constructeur(constructeurs[i]);
+        }
+    }
+
+
+    @Test void testAccessibilite(){
+        for (Constructeur constructeur : this.constructeur) {
+            switch (constructeur.getParametre().size()) {
+                case 0 -> assertEquals("protected", constructeur.getAccessibilite());
+                case 1 -> assertEquals("private", constructeur.getAccessibilite());
+                case 2 -> assertEquals("public", constructeur.getAccessibilite());
+                case 3 -> assertEquals("default", constructeur.getAccessibilite());
+                default -> {
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testParametre(){
+        for (Constructeur construc : constructeur) {
+            switch (construc.getAccessibilite()) {
+                case "default" -> {
+                    ArrayList<Parametre> parametres1 = construc.getParametre();
+                    assertEquals(3, parametres1.size());
+                    assertEquals("double", parametres1.get(0).getType());
+                    assertEquals("double", parametres1.get(1).getType());
+                    assertEquals("double", parametres1.get(2).getType());
+                }
+                case "public" -> {
+                    ArrayList<Parametre> parametres2 = construc.getParametre();
+                    assertEquals(2, parametres2.size());
+                    assertEquals("int", parametres2.get(0).getType());
+                    assertEquals("int", parametres2.get(1).getType());
+                }
+                case "private" -> {
+                    ArrayList<Parametre> parametres3 = construc.getParametre();
+                    assertEquals(1, parametres3.size());
+                    assertEquals("int", parametres3.get(0).getType());
+                }
+                case "protected" -> {
+                    ArrayList<Parametre> parametres4 = construc.getParametre();
+                    assertEquals(0, parametres4.size());
+                }
+                default -> {
+                }
+            }
+        }
+    }
+
+}
