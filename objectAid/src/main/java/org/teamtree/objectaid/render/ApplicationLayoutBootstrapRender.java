3a34e6ae9fe5b1d565d335b3651a5e611f22c957
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-01-05 17:55:18 +0100

    feat(importation): directory chooser

--- objectAid/src/main/java/org/teamtree/objectaid/render/ApplicationLayoutBootstrapRender.java
+++ objectAid/src/main/java/org/teamtree/objectaid/render/ApplicationLayoutBootstrapRender.java
@@ -3,6 +3,7 @@ package org.teamtree.objectaid.render;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.DirectoryChooser;
 import org.teamtree.objectaid.Fabrique.SceneFactory;
 import org.teamtree.objectaid.MVC.Model.ApplicationState;
 import org.teamtree.objectaid.MVC.Model.Model;
@@ -16,7 +17,12 @@ public class ApplicationLayoutBootstrapRender implements SceneFactory {
         this.base = new BorderPane();
 
         final var button = new Button("Charger un projet");
-        button.setOnAction(event -> model.setApplicationState(ApplicationState.PROJECT_LOADED));
+        button.setOnAction(event -> {
+            final var fileChooser = new DirectoryChooser();
+            fileChooser.setTitle("Ouvrir un projet");
+
+            final var file = fileChooser.showDialog(null);
+        });
 
         base.setCenter(button);
     }
