235c2283bf8f78de4e4fd7d3e84b401175637037
Grossmann Jeremy <jeremy.grossmann4@etu.univ-lorraine.fr>
2023-01-06 10:56:50 +0100

    javadoc de la classe VueButtonBarClasse

--- objectAid/src/main/java/org/teamtree/objectaid/MVC/Vue/VueButtonBarClasse.java
+++ objectAid/src/main/java/org/teamtree/objectaid/MVC/Vue/VueButtonBarClasse.java
@@ -4,17 +4,29 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import org.teamtree.objectaid.MVC.Controller.ControllerButtonGeneral;
 import org.teamtree.objectaid.MVC.Model.Model;
-import org.teamtree.objectaid.MVC.Vue.Observateur;
 
+/** Vue utilisée pour les boutons d'affichage d'une classe */
 public class VueButtonBarClasse extends HBox implements Observateur {
 
+    /** Attribut qui represente le bouton pour afficher/masquer les attributs de la classe */
     private Button buttonAttributs;
+
+    /** Attribut qui represente le bouton pour afficher/masquer les methodes de la classe */
     private Button buttonMethodes;
+
+    /** Attribut qui represente le bouton pour afficher/masquer les constructeurs de la classe */
     private Button buttonConstructeurs;
+
+    /** Attribut qui represente le booleen permettant de savoir quand la vue est censée afficher les boutons */
     private boolean afficherButtonBarClasse;
 
+    /** Attribut qui represente le modele */
     private Model model;
 
+    /**
+     * Constructeur de la classe
+     * @param model modele
+     */
     public VueButtonBarClasse(Model model) {
         this.model = model;
         buttonAttributs = new Button("Attributs");
@@ -26,6 +38,9 @@ public class VueButtonBarClasse extends HBox implements Observateur {
         afficherButtonBarClasse = model.getBarreBoutonsSpecifique();
     }
 
+    /**
+     * Methode qui permet de mettre à jour la vue
+     */
     @Override
     public void actualiser() {
         afficherButtonBarClasse = model.getBarreBoutonsSpecifique();
