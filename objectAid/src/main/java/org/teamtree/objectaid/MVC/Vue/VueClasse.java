8a8a603c873d9c08fba7ecced3dea4f134c7c3fb
Grossmann Jeremy <jeremy.grossmann4@etu.univ-lorraine.fr>
2023-01-06 11:25:54 +0100

    javadoc de la classe VueClasse : modification et ajout

--- objectAid/src/main/java/org/teamtree/objectaid/MVC/Vue/VueClasse.java
+++ objectAid/src/main/java/org/teamtree/objectaid/MVC/Vue/VueClasse.java
@@ -20,7 +20,11 @@ public class VueClasse extends Pane implements Observateur {
 
     /** Modèle */
     private final Model model;
+
+    /** HashMap qui represente la liste des classes à afficher, chaque classe possede comme clé le nom de la classe */
     private final HashMap<String, VueClasseAffichage> classes;
+
+    /** Liste des fleches */
     private final List<VueFleche> fleches;
 
 
