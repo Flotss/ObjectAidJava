32720aa96ab37c0612a3df4cb0eb2be074346744
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-01-09 16:25:26 +0100

    feat(importation): remove compile part

--- objectAid/src/main/java/org/teamtree/objectaid/service/ProjectCompilationProcessService.java
+++ objectAid/src/main/java/org/teamtree/objectaid/service/ProjectCompilationProcessService.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ProjectCompilationProcessService {
     private final Model model;
@@ -26,61 +25,41 @@ public class ProjectCompilationProcessService {
 
     public void compileProject(final Path path) {
         if (path.toFile().isDirectory()) {
-            try {
-                final var temporaryOutputDirectory = Files.createTempDirectory("objectaid");
-                final var tempFile = temporaryOutputDirectory.toFile();
+            final var files = Arrays.stream(Objects.requireNonNull(path.toFile().listFiles())).filter(File::isFile).filter(FileExtension::isClassFile).toList();
 
-                compile(path, temporaryOutputDirectory);
+            files.forEach(file -> {
+                try {
+                    final var loader = new CLoader();
+                    final var clazz = loader.loadClass(file.getAbsolutePath());
 
-                final var files = Arrays.stream(Objects.requireNonNull(path.toFile().listFiles())).filter(File::isFile).filter(FileExtension::isClassFile).toList();
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            });
 
-                files.forEach(file -> {
-                    // sout current dir
-                    System.out.println("Current dir: " + file.getAbsolutePath());
-                    // sout current file
-                    System.out.println("Current file: " + file.getName());
-
-                    // get relative path from path
-                    final String relativePath;
-                    // url to file
-
-                    try {
-                        final var loader = new CLoader();
-                        final var clazz = loader.loadClass(file.getAbsolutePath());
-
-                        System.out.println(clazz.getName());
-                        System.out.println(clazz.getModifiers());
-
-                    } catch (ClassNotFoundException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
-    void compile(Path source, Path target) {
+    /**
+     void compile(Path source, Path target) {
 
-        // clear target dir
-        target.toFile().deleteOnExit();
+     // clear target dir
+     target.toFile().deleteOnExit();
 
-        final var start = System.currentTimeMillis();
+     final var start = System.currentTimeMillis();
 
-        try {
-            final var javaFiles = Files.walk(source).filter(Files::isRegularFile).filter(p -> p.getFileName().toString().endsWith(FileExtension.JAVA_EXTENSION)).map(Path::toAbsolutePath).map(Path::toString).toList();
+     try {
+     final var javaFiles = Files.walk(source).filter(Files::isRegularFile).filter(p -> p.getFileName().toString().endsWith(FileExtension.JAVA_EXTENSION)).map(Path::toAbsolutePath).map(Path::toString).toList();
 
-            final var args = Stream.concat(Stream.of("javac", "-cp", source.toString()), javaFiles.stream()).toArray(String[]::new);
-            final var process = new ProcessBuilder(args).redirectErrorStream(true).inheritIO().start();
-            process.waitFor();
+     final var args = Stream.concat(Stream.of("javac", "-cp", source.toString()), javaFiles.stream()).toArray(String[]::new);
+     final var process = new ProcessBuilder(args).redirectErrorStream(true).inheritIO().start();
+     process.waitFor();
 
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+     } catch (IOException | InterruptedException e) {
+     throw new RuntimeException(e);
+     }
 
-    }
+     }
 
-
+     */
 }
