32702717c440b9161c64f3b270b56978b84e7a71
Flotss <manginf54@gmail.com>
2023-01-10 09:02:06 +0100

    l'affichage des classes cachés est maintenant dans une fonction

--- objectAid/src/main/java/org/teamtree/objectaid/MVC/Controller/ControllerButtonGeneral.java
+++ objectAid/src/main/java/org/teamtree/objectaid/MVC/Controller/ControllerButtonGeneral.java
@@ -57,29 +57,32 @@ public class ControllerButtonGeneral implements EventHandler<ActionEvent> {
                 model.afficherRelations(relationsGeneralesAffiche);
                 break;
             case "Classe cachée" :
-                Stage stage = new Stage();
-                VBox vBox = new VBox();
-                for (var classe : model.getHiddenClasses()) {
-                    Label label = new Label(classe.getNom());
-                    label.setFont(new Font(20));
-                    label.setPadding(new Insets(10));
-                    label.setOnMouseClicked(new ControllerButtonClasseCachee(model, classe));
-                    vBox.getChildren().add(label);
-                }
-                if (vBox.getChildren().isEmpty()) {
-                    Label label = new Label("Aucune classe cachée");
-                    label.setFont(new Font(20));
-                    label.setPadding(new Insets(10));
-                    vBox.getChildren().add(label);
-                }
-
-                stage.setScene(new Scene(vBox));
-                stage.setX(800);
-                stage.setAlwaysOnTop(true);
-                stage.setMaxWidth(300);
-                stage.show();
+                afficherClasseCachee();
                 break;
         }
+    }
 
+    private void afficherClasseCachee(){
+        Stage stage = new Stage();
+        VBox vBox = new VBox();
+        for (var classe : model.getHiddenClasses()) {
+            Label label = new Label(classe.getNom());
+            label.setFont(new Font(20));
+            label.setPadding(new Insets(10));
+            label.setOnMouseClicked(new ControllerButtonClasseCachee(model, classe));
+            vBox.getChildren().add(label);
+        }
+        if (vBox.getChildren().isEmpty()) {
+            Label label = new Label("Aucune classe cachée");
+            label.setFont(new Font(20));
+            label.setPadding(new Insets(10));
+            vBox.getChildren().add(label);
+        }
+
+        stage.setScene(new Scene(vBox));
+        stage.setX(800);
+        stage.setAlwaysOnTop(true);
+        stage.setMaxWidth(300);
+        stage.show();
     }
 }
