c094b6315603997f53d228081d62160b531cc363
Flotss <manginf54@gmail.com>
2023-01-12 09:15:23 +0100

    Début compilationUml + refactor

--- objectAid/src/main/java/org/teamtree/objectaid/MVC/Controller/ControllerButtonGeneral.java
+++ objectAid/src/main/java/org/teamtree/objectaid/MVC/Controller/ControllerButtonGeneral.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.teamtree.objectaid.MVC.Model.Model;
+import org.teamtree.objectaid.ObjectAidApplication;
 
 /**
  * Classe qui permet de gérer tous les boutons de l'application
@@ -57,33 +58,10 @@ public class ControllerButtonGeneral implements EventHandler<ActionEvent> {
                 relationsGeneralesAffiche = !relationsGeneralesAffiche;
                 model.afficherRelations(relationsGeneralesAffiche);
                 break;
-            case "Classe cachée" :
-                afficherClasseCachee();
+            case "Supprimer les classes":
+                model.supprimerClassesAffichage();
+                ObjectAidApplication.menuBar.getMenus().get(1).getItems().clear();
                 break;
         }
-    }
-
-    private void afficherClasseCachee(){
-        Stage stage = new Stage();
-        VBox vBox = new VBox();
-        for (var classe : model.getHiddenClasses()) {
-            Label label = new Label(classe.getNom());
-            label.setFont(new Font(20));
-            label.setPadding(new Insets(10));
-            label.setOnMouseClicked(new ControllerButtonClasseCachee(model, classe));
-            vBox.getChildren().add(label);
-        }
-        if (vBox.getChildren().isEmpty()) {
-            Label label = new Label("Aucune classe cachée");
-            label.setFont(new Font(20));
-            label.setPadding(new Insets(10));
-            vBox.getChildren().add(label);
-        }
-
-        stage.setScene(new Scene(vBox));
-        stage.setX(800);
-        stage.setAlwaysOnTop(true);
-        stage.setMaxWidth(300);
-        stage.show();
     }
 }
